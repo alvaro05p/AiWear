4d36985ad43852c4365fc69269663d388a21b34c
alvaro05p <alvaro05pomg@gmail.com>
2025-09-13 12:44:56 +0200

    primer commit AiWear

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header"
+import Hero from "./Hero"
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <Hero></Hero>
+    </>
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,22 @@
+export default function Header() {
+  return (
+    <header className="text-gray-600 body-font">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0" href="#">
+            <img src="logoNoBackground.png" alt="" className="w-10 h-10 text-white rounded-full"/>
+            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+          <span className="ml-3 text-xl">AiWear</span>
+        </a>
+        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+          <a className="mr-5 hover:text-gray-900" href="#">Shirts</a>
+          <a className="mr-5 hover:text-gray-900" href="#">Hoodies</a>
+          <a className="mr-5 hover:text-gray-900" href="#">Create</a>
+          <a className="mr-5 hover:text-gray-900" href="#">User designs</a>
+        </nav>
+        <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+          <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="currentColor"  class="icon icon-tabler icons-tabler-filled icon-tabler-shopping-cart"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 2a1 1 0 0 1 .993 .883l.007 .117v1.068l13.071 .935a1 1 0 0 1 .929 1.024l-.01 .114l-1 7a1 1 0 0 1 -.877 .853l-.113 .006h-12v2h10a3 3 0 1 1 -2.995 3.176l-.005 -.176l.005 -.176c.017 -.288 .074 -.564 .166 -.824h-5.342a3 3 0 1 1 -5.824 1.176l-.005 -.176l.005 -.176a3.002 3.002 0 0 1 1.995 -2.654v-12.17h-1a1 1 0 0 1 -.993 -.883l-.007 -.117a1 1 0 0 1 .883 -.993l.117 -.007h2zm0 16a1 1 0 1 0 0 2a1 1 0 0 0 0 -2zm11 0a1 1 0 1 0 0 2a1 1 0 0 0 0 -2z" /></svg>
+        </button>
+      </div>
+    </header>
+  )
+}
